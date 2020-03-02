1474105b60f023f0a3207e75e1d1134ba192a8e4
Farzad Daud <daud.farzad1@gmail.com>
2020-03-02 15:26:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,12 @@
 
 def
-  
+    library = YAML.load_file(file_path)
+   result = {"get_meaning" => {}, "get_emoticon" => {}}
+   library.each do |meaning, emoticons|
+     result["get_meaning"][emoticons[1]] = meaning
+     result["get_emoticon"][emoticons[0]] = emoticons[1]
+   end
+   result
 
 
 end
