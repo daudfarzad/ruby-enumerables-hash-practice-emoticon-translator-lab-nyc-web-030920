dd170b7b6c3602b45fc2738fc83e8b6434a09844
Farzad Daud <daud.farzad1@gmail.com>
2020-03-02 15:33:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,7 @@
-
-def load_library(path)
-  emoticons = YAML.load_file(path)
-  emoticon_hash = Hash.new
-
-  emoticon_hash["get_emoticon"] = Hash.new
-  emoticon_hash["get_meaning"] = Hash.new
-
-  emoticons.each do |english_word, emoticon_set|
-    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
-  end
-  emoticon_hash
-end
-
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+	 library = load_library(file_path)
+   if library["get_meaning"].include?(japanese_emoticon) 
+     library["get_meaning"][japanese_emoticon]
+   else
+     "Sorry, that emoticon was not found"
+   end
